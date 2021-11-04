9a048c6a7d96a2356009fb498c7264746d88b17a
Matthew Gelder <mattymozzah@gmail.com>
2021-11-04 16:18:26 +0000

    fourth commit

--- app/js/cart-drawer.js
+++ app/js/cart-drawer.js
@@ -1,26 +1,61 @@
 var cartAmount = 0;
+var addToCartCont = document.getElementById('cart--drawer');
+var miniCartCont = document.getElementById('mini-cart--drawer');
+var addToCartText = document.getElementById('addToCart');
+var productPrice = 22.81;
+var cartPrice = document.getElementById('cart__drawer_price');
+var quantityText = document.getElementById('cart__quantity');
+var cartCounter = document.getElementById('nav__cart_counter');
+
 function addToCart() {
-  var addToCart = document.getElementById('cart--drawer');
-  var cartCounter = document.getElementById('nav__cart_counter');
-  var addToCartText = document.getElementById('addToCart');
-  var quantityText = document.getElementById('cart__quantity');
   addToCartText.innerHTML = '<div class="block__cart_loader_cont"><div class="block__cart_loader"></div></div>';
-    setTimeout (function() {
-      
-      addToCart.classList.add('drawer--active');
-      addToCartText.innerHTML = '<p class="block__product_button button__format">ADDED</p>';
-    },
-    1000);
-    
-    cartAmount++;
-    cartCounter.innerHTML = cartAmount;
-    quantityText.innerHTML = "Quantity: "+cartAmount;
+    openCartDrawer();
+    updateCartCounter();
   }
 
-function closeDrawer() {
-    var addToCart = document.getElementById('cart--drawer');
-    var addToCartText = document.getElementById('addToCart');
-    addToCart.classList.remove('drawer--active');
-    addToCartText.innerHTML = '<p class="block__product_button button__format">ADD TO CART</p>';
+  function openCartDrawer() {
+    setTimeout (function() {  
+      addToCartCont.classList.add('drawer--active');
+      addToCartText.innerHTML = 'ADDED';
+      document.getElementById("cart--drawer-confirmation").style.display = "block";
+    },
+    1000);
+  }
+  
+  function updateCartCounter() {  
+    cartAmount++;
+    updateQuantity();
+  }
+
+  function decreaseCartCounter() {
+    cartAmount--;
+    updateQuantity();
+
+    if (cartAmount == 0) {
+      setTimeout (function() {  
+        closeDrawer();
+      },
+      1000);
+    }
+  }
+
+function openCart() {
+  addToCartCont.classList.add('drawer--active');
+  document.getElementById("cart--drawer-confirmation").style.display = "none";
+}
+ 
+function updateQuantity() {
+  cartCounter.innerHTML = cartAmount;
+  quantityText.innerHTML = "Quantity: "+cartAmount;
+  cartPriceTotal = productPrice * cartAmount;
+  cartPrice.innerHTML = "Total: £"+cartPriceTotal.toFixed(2);
 }  
-    
+
+function closeDrawer() {
+    addToCartCont.classList.remove('drawer--active');
+    addToCartText.innerHTML = 'ADD TO CART';
+} 
+
+
+
+     
