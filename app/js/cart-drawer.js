9e3acb59778a0851903ac5d3ad015ee3b29c3102
Matthew Gelder <mattymozzah@gmail.com>
2021-11-04 00:12:05 +0000

    second commit

--- app/js/cart-drawer.js
+++ app/js/cart-drawer.js
@@ -1,8 +1,9 @@
-
+var cartAmount = 0;
 function addToCart() {
   var addToCart = document.getElementById('cart--drawer');
   var cartCounter = document.getElementById('nav__cart_counter');
   var addToCartText = document.getElementById('addToCart');
+  var quantityText = document.getElementById('cart__quantity');
   addToCartText.innerHTML = '<div class="block__cart_loader_cont"><div class="block__cart_loader"></div></div>';
     setTimeout (function() {
       
@@ -10,8 +11,10 @@ function addToCart() {
       addToCartText.innerHTML = '<p class="block__product_button button__format">ADDED</p>';
     },
     1000);
-
-    cartCounter.innerHTML = '1';
+    
+    cartAmount++;
+    cartCounter.innerHTML = cartAmount;
+    quantityText.innerHTML = "Quantity: "+cartAmount;
   }
 
 function closeDrawer() {
